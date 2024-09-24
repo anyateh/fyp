fbf13bb67c875156a5a6cb2d6557e812c57fdebc
snqzspg <snqzspg@gmail.com>
2024-09-25 05:14:30 +0800

    Adjusted setting up of listening from receiveing socket

--- timing-ping/udp_echo/udp_echo_time.py
+++ timing-ping/udp_echo/udp_echo_time.py
@@ -14,25 +14,23 @@ _logger_console_handler.setFormatter(logging.Formatter(
 ))
 _logger.setLevel(logging.DEBUG)
 
-def send_msg(hostname_ip:str, port:int, msg:bytes) -> None:
-	sock = socket(AF_INET, SOCK_DGRAM)
-	_logger.info(f"Sending message to {hostname_ip}:{port}")
-	sock.sendto(msg, (hostname_ip, port))
-
 def udp_echo_time_sender(sender_hostname_ip:str, sender_port:int, receiver_hostname_ip:str, receiver_port:int) -> None:
 	unix_time_str = str(floor(time()))
-	sock = socket(AF_INET, SOCK_DGRAM)
+
+	sock  = socket(AF_INET, SOCK_DGRAM)
+
+	rsock = socket(AF_INET, SOCK_DGRAM)
+	rsock.bind((sender_hostname_ip, sender_port))
+	rsock.setblocking(True)
+	rsock.settimeout(3)
+
 	_logger.info(f"Sending the unix_timestamp to {receiver_hostname_ip}:{receiver_port}")
 	_logger.debug(f"UNIX timestamp str -> {unix_time_str}")
 	t0 = time()
 	sock.sendto(unix_time_str.encode(encoding = "utf-8"), (receiver_hostname_ip, receiver_port))
 
-	sock = socket(AF_INET, SOCK_DGRAM)
-	sock.bind((sender_hostname_ip, sender_port))
-
-	sock.setblocking(True)
 	_logger.info(f"Awaiting response via {sender_hostname_ip}:{sender_port}...")
-	received_msg, sending_addr = sock.recvfrom(1024)
+	received_msg, sending_addr = rsock.recvfrom(1024)
 	t1 = time()
 	time_taken = t1 - t0
 
