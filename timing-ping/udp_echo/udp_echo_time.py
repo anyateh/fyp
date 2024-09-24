cd451562522b91da37a74b631744f20f12163816
snqzspg <snqzspg@gmail.com>
2024-09-25 05:03:00 +0800

    Added time measurement

--- timing-ping/udp_echo/udp_echo_time.py
+++ timing-ping/udp_echo/udp_echo_time.py
@@ -24,6 +24,7 @@ def udp_echo_time_sender(sender_hostname_ip:str, sender_port:int, receiver_hostn
 	sock = socket(AF_INET, SOCK_DGRAM)
 	_logger.info(f"Sending the unix_timestamp to {receiver_hostname_ip}:{receiver_port}")
 	_logger.debug(f"UNIX timestamp str -> {unix_time_str}")
+	t0 = time()
 	sock.sendto(unix_time_str.encode(encoding = "utf-8"), (receiver_hostname_ip, receiver_port))
 
 	sock = socket(AF_INET, SOCK_DGRAM)
@@ -32,8 +33,11 @@ def udp_echo_time_sender(sender_hostname_ip:str, sender_port:int, receiver_hostn
 	sock.setblocking(True)
 	_logger.info(f"Awaiting response via {sender_hostname_ip}:{sender_port}...")
 	received_msg, sending_addr = sock.recvfrom(1024)
+	t1 = time()
+	time_taken = t1 - t0
 
 	_logger.info(f"Received message: '{received_msg}' from {sending_addr}")
+	_logger.info(f"Time measured: {time_taken} seconds")
 
 def udp_echo_time_receiver(sender_hostname_ip:str, sender_port:int, receiver_hostname_ip:str, receiver_port:int) -> None:
 	sock = socket(AF_INET, SOCK_DGRAM)
