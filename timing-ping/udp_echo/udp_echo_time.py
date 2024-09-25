528139f34954b77e077266181b599e9b78850d30
snqzspg <snqzspg@gmail.com>
2024-09-25 19:55:47 +0800

    Added timeout feedback

--- timing-ping/udp_echo/udp_echo_time.py
+++ timing-ping/udp_echo/udp_echo_time.py
@@ -12,7 +12,7 @@ _logger_console_handler.setFormatter(logging.Formatter(
 ))
 _logger.setLevel(logging.DEBUG)
 
-def udp_echo_time_sender(echo_to_hostname_ip:str, echo_to_port:int, own_hostname_ip:str, listening_port:int) -> tuple[str, float]:
+def udp_echo_time_sender(echo_to_hostname_ip:str, echo_to_port:int, own_hostname_ip:str, listening_port:int) -> tuple[str, bool, float]:
 	unix_time_str   = str(floor(time()))
 	unix_time_bytes = unix_time_str.encode("utf-8")
 
@@ -27,14 +27,19 @@ def udp_echo_time_sender(echo_to_hostname_ip:str, echo_to_port:int, own_hostname
 	t0 = time()
 	sock.sendto(unix_time_bytes, (echo_to_hostname_ip, echo_to_port))
 
-	_logger.info(f"Awaiting response via {own_hostname_ip}:{listening_port}...")
-	received_msg, (from_hostname_ip, from_port) = sock.recvfrom(1024)
-	t1 = time()
-	time_taken = t1 - t0
+	try:
+		_logger.info(f"Awaiting response via {own_hostname_ip}:{listening_port}...")
+		received_msg, (from_hostname_ip, from_port) = sock.recvfrom(1024)
+		t1 = time()
+		time_taken = t1 - t0
 
-	_logger.info(f"Received message: '{received_msg}' from {from_hostname_ip}:{from_port}")
-	_logger.info(f"Time measured: {time_taken} seconds")
-	return unix_time_str, time_taken
+		_logger.info(f"Received message: '{received_msg}' from {from_hostname_ip}:{from_port}")
+		_logger.info(f"Time measured: {time_taken} seconds")
+		return unix_time_str, True, time_taken
+	except TimeoutError:
+		_logger.info(f"Timed out waiting response via {own_hostname_ip}:{listening_port}...")
+		_logger.info("No response")
+		return unix_time_str, False, 0.0
 
 def udp_echo_time_receiver(own_hostname_ip:str, own_port:int, echo_back_port:int) -> None:
 	sock = socket(AF_INET, SOCK_DGRAM)
