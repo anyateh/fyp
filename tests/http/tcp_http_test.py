fe7d5bb13b2ed09e0055d17b98fc1512aaf73571
snqzspg <snqzspg@gmail.com>
2024-10-06 21:40:52 +0800

    Implemented a more logically accurate checker for HTTP completeness

--- tests/http/tcp_http_test.py
+++ tests/http/tcp_http_test.py
@@ -42,6 +42,93 @@ def start_server() -> socket:
 
 	return sock
 
+def create_http_response(code:int, code_msg:str, content_type:str, content:bytes, headers:Optional[dict[str,str]] = None) -> bytes:
+	res_headers  = f"HTTP/1.1 {code} {code_msg}\r\n"
+	res_headers += f"Content-Type: {content_type}\r\n"
+	res_headers += f"Content-Length: {len(content)}\r\n"
+	if headers:
+		for h, v in headers.items():
+			res_headers += f"{h}: {v}\r\n"
+	res_headers += "\r\n"
+
+	return res_headers.encode(encoding = "utf-8") + content
+
+def _find_seq_index(bytes_list:list[int], seq:list[int]) -> int:
+	seq_len = len(seq)
+	seq_pos = 0
+	for potential_patt in (bytes_list[i:i + seq_len] for i in range(len(bytes_list) - seq_len + 1)):
+		if potential_patt == seq:
+			return seq_pos
+		seq_pos += 1
+	
+	return -1
+
+# Returns -1 if headers are incomplete
+def _get_headers_len(received_info:list[int]) -> int:
+	seq = [13, 10, 13, 10]
+	seq_pos = 0
+	for potential_patt in (received_info[i:i + 4] for i in range(len(received_info) - 3)):
+		if potential_patt == seq:
+			return seq_pos
+		seq_pos += 1
+	
+	return -1
+
+def _get_content_len(http_req_res:list[int]) -> Optional[int]:
+	len_line = None
+	content_len_header = list(b'Content-Length:')
+
+	def split_int_iter_by_int(int_seq:Iterable[int], n:int) -> Iterator[list[int]]:
+		spl = []
+		for i in int_seq:
+			spl.append(i)
+			if i == n:
+				yield spl
+				spl = []
+
+	def int_list_begins_with(ilist:list[int], patt:list[int]) -> bool:
+		if len(patt) > len(ilist):
+			return False
+		
+		return ilist[:len(patt)] == patt
+
+	for line in split_int_iter_by_int(http_req_res, 10):
+		if int_list_begins_with(line, content_len_header):
+			len_line = line
+			break
+
+	if not len_line:
+		return None
+
+	l = int("".join(chr(i) for i in len_line if 48 <= i < 58))
+
+	return l
+
+def is_http_in_header(http_req:list[int]) -> bool:
+	first_crlf = _find_seq_index(http_req, [13, 10])
+	if first_crlf < 4:
+		return False
+	
+	for segment in (http_req[i:i + 4] for i in range(len(http_req[:first_crlf]) - 3)):
+		if segment == list(b'HTTP'):
+			return True
+
+	return False
+
+def _is_complete_http_req(received_info:list[int]) -> bool:
+	if not is_http_in_header(received_info):
+		return False
+	
+	headers_len = _get_headers_len(received_info)
+	if headers_len == -1:
+		return False
+
+	content_len = _get_content_len(received_info)
+	if not content_len:
+		content_len = 0
+
+	return len(received_info) == content_len + headers_len + 4
+
 def listen_for_a_http_req(sock:socket) -> None:
 	client_connect, (client_ip, client_port) = sock.accept()
 
@@ -50,12 +137,14 @@ def listen_for_a_http_req(sock:socket) -> None:
 	request_bytes = []
 
 	# header_end_detected = lambda packet: packet.decode(encoding = "utf-8").find("\r\n\r\n") != -1
-	header_end_detected = lambda : request_bytes[-4:] == [13, 10, 13, 10]
+	# header_end_detected = lambda : request_bytes[-4:] == [13, 10, 13, 10]
+
+	client_connect.setblocking(False)
 
 	_logger.debug(f"Attempting to read from {client_ip}:{client_port}...")
 	data = client_connect.recv(1024)
 	request_bytes.extend(list(data))
-	while not header_end_detected():
+	while not _is_complete_http_req(request_bytes):
 		_logger.debug(f"Attempting to read from {client_ip}:{client_port}...")
 		data = client_connect.recv(1024)
 		if not data:
