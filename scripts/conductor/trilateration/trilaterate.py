f16075272eb300cb46edb6ef8e7ea6f6052b44d2
snqzspg <snqzspg@gmail.com>
2025-04-20 15:29:21 +0800

    Adjusted assert conditions for trilateration

--- scripts/conductor/trilateration/trilaterate.py
+++ scripts/conductor/trilateration/trilaterate.py
@@ -28,7 +28,7 @@ def estimate_location(antennas:dict[int, AntennaNode], use_avg:bool = False) -> 
 		remaining_antennas = {k:v for k, v in antennas.items() if v.dbm is not None}
 		del remaining_antennas[ref_ant_id]
 
-		# assert len(remaining_antennas) >= 2
+		assert len(remaining_antennas) >= 1
 
 		get_inv_frii_fx = lambda ant: ant.inv_friis_avg.avg() if use_avg else ant.inverse_friis()
 
@@ -45,6 +45,8 @@ def estimate_location(antennas:dict[int, AntennaNode], use_avg:bool = False) -> 
 			y1_m_y0 = second_antenna.y - ref_ant.y
 			x1_m_x0 = second_antenna.x - ref_ant.x
 
+			assert y1_m_y0 != 0 or x1_m_x0 != 0
+
 			if y1_m_y0 == 0:
 				x_estimate = tri_mat_rhs / (2 * x1_m_x0)
 				return x_estimate, ref_ant.y
@@ -55,7 +57,9 @@ def estimate_location(antennas:dict[int, AntennaNode], use_avg:bool = False) -> 
 
 			lingrad_between_ants = y1_m_y0 / x1_m_x0
 
-			x_estimate = (tri_mat_rhs/(2 * y1_m_y0) + ref_ant.x*lingrad_between_ants - ref_ant.y) / (lingrad_between_ants + 1/lingrad_between_ants)
+			x_estimate = (tri_mat_rhs/(2 * y1_m_y0)              \
+				 + ref_ant.x*lingrad_between_ants - ref_ant.y)   \
+				   / (lingrad_between_ants + 1/lingrad_between_ants)
 
 			return x_estimate, ((x_estimate - ref_ant.x) * lingrad_between_ants + ref_ant.y)
 
@@ -63,7 +67,9 @@ def estimate_location(antennas:dict[int, AntennaNode], use_avg:bool = False) -> 
 			[i.x * 2 - x0_x_2, i.y * 2 - y0_x_2]
 			for i in remaining_antennas.values()
 		])
-		right_side_matrix_rows = np.array([matrix_rhs(i) for i in remaining_antennas.values()])
+		right_side_matrix_rows = np.array([
+			matrix_rhs(i) for i in remaining_antennas.values()
+		])
 
 		return tuple(np.linalg.lstsq(left_side_matrix_rows, right_side_matrix_rows, rcond = None)[0])
 
